a52c7e0e171b65179eb59f8357bfaceefc8edbd1
“Irina <ira.bel4ikova@gmail.com>
2024-01-29 02:56:58 +0400

    feat: contain number

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -178,8 +178,14 @@ function getIndexOf(/* str, letter */) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const numberInString = String(num);
+  for (let i = 0; i < numberInString.length; i += 1) {
+    if (numberInString[i] === String(digit)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
